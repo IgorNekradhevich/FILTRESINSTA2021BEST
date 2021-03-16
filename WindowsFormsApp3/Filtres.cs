2f29f8eb7ae3b7faaa323762f3a941892cec85a0
Игорь Некрашевич <oikmaster@yandex.ru>
2021-03-16 17:37:39 +0700

    16/03

---
+++ WindowsFormsApp3/Filtres.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    class Filtres
+    {
+        Bitmap bitmap;
+        public Filtres(string path)
+        {
+            bitmap = new Bitmap(path);
+        }
+
+        public Bitmap AllGrayLikeMyLife()
+        {
+            int w = bitmap.Width;
+            int h = bitmap.Height;
+            Bitmap gray = new Bitmap(w, h);
+            for (int i=0; i<w;i++)
+            {
+                for (int j=0; j<h;j++)
+                {
+                    Color pixel = bitmap.GetPixel(i, j);
+                    gray.SetPixel(i, j, Color.FromArgb(pixel.G, pixel.G, pixel.G));
+                }
+            }
+            return gray;
+        }
+        public Bitmap GiveMeYourHeart()
+        {
+            return bitmap; 
+        }
+    }
+}
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Filtres igorbest = new Filtres(openFileDialog1.FileName);
+            pictureBox1.Image = igorbest.AllGrayLikeMyLife();
+        }
+    }
+}
